798b62f896c052857818f4967525910bd7b55f86
Francesco Ceccon <francescoceccon@gmail.com>
2023-07-25 12:38:38 -0300

    teste

---
+++ Infrastructure/Services/Interfaces/IUrlService.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services.Interfaces
+{
+    public interface IURLService
+    {
+        Task ProcessMottuUrlsAsync(List<MottuUrl> mottuUrls);
+        Task<MottuUrl> ProcessShortUrlAsync(string longUrl);
+        Task UpdateAsync(MottuUrlDTO mottuUrl);
+        Task<MottuUrl> ReadUrlAsync(string url);
+        Task UpdateAsync(MottuUrl mottuUrl);
+        Task<MottuUrl> GetUrlAsync(string id);
+        Task DeleteUrlAsync(string id);
+    }
+}
